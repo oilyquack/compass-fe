ff179256b8a8ec941d27b0b36c979ef7d9b631ba
Ollie Crook <ollie@hubblehq.com>
2025-04-30 19:43:37 +0100

    Add link to mailing list on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Compass Festival</h1>
+
+      <br />
+
+      <Link href="/mailing-list" target="_blank">
+        Join the mailing list
+      </Link>
     </div>
   );
 }
