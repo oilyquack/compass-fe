de86a52e581c06c2f4de69fe440cf8bc651988e5
Ollie Crook <ollie@hubblehq.com>
2025-04-30 22:26:27 +0100

    Remove commented code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 
-// import Link from "next/link";
-
 import "./globals.css";
 import styles from "./layout.module.css";
 
@@ -25,31 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.variable} ${styles.Layout}`}>
-        {/* <nav className={styles["Layout-nav"]}>
-          <ul className={styles["Layout-navItems"]}>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <a href="https://example.com">Apply to play</a>
-            </li>
-            <li>
-              <Link href="/mailing-list">Join the mailing list</Link>
-            </li>
-            <li>
-              <a href="/archive">2024</a>
-            </li>
-          </ul>
-        </nav> */}
-
         <main className={styles["Layout-main"]}>{children}</main>
-
-        {/* <footer className={styles["Layout-footer"]}>
-          <p>Copyright &copy; Compass {new Date().getFullYear()}</p>
-        </footer> */}
       </body>
     </html>
   );
