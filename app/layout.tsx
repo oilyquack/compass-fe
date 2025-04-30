7c6136a79cf242998d3998585962d60ffb34b309
Ollie Crook <ollie@hubblehq.com>
2025-04-30 22:37:52 +0100

    Try to fix svg compression on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+
       <body className={`${montserrat.variable} ${styles.Layout}`}>
         <main className={styles["Layout-main"]}>{children}</main>
       </body>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Exclude SVGs from being processed by default file loader
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/, // Apply only to JavaScript/TypeScript files
+      use: ["@svgr/webpack"], // Use SVGR to handle SVGs as React components
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
