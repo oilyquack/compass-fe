c512369d5f6558e44d24dcc00561106e21dea098
Ollie Crook <olliejcrook@gmail.com>
2025-07-02 13:25:22 +0100

    Fix some spacing issues

--- app/artist/[slug]/page.tsx
+++ app/artist/[slug]/page.tsx
@@ -1,9 +1,12 @@
+import NextImage from "next/image";
 import { notFound } from "next/navigation";
 
 import artists from "../../../data/artists.json";
 import MusicPlayer from "./components/MusicPlayer";
 import SocialLinks from "./components/SocialLinks";
 import styles from "./Artist.module.css";
+
+import Section from "../../components/Section";
 
 export async function generateStaticParams() {
   return artists.map((artist) => ({ slug: artist.slug }));
@@ -22,23 +25,29 @@ export default async function ArtistPage({
   }
 
   return (
-    <div className={styles.Artist}>
-      <img
-        alt={artist.name}
-        className={`${styles["Artist-image"]} mg-btm-lg`}
-        src={`/images/artists/${artist.slug}/main.jpg`}
-      />
-
-      <div className={styles["Artist-content"]}>
-        <h1 className="mg-btm-md">{artist.name}</h1>
-        <div className="mg-btm-md">
-          <SocialLinks artist={artist} />
+    <Section>
+      <div className={styles.Artist}>
+        <div className={`${styles["Artist-imageWrapper"]} mg-btm-lg`}>
+          <NextImage
+            alt={artist.name}
+            className={styles["Artist-image"]}
+            fill
+            src={`/images/artists/${artist.slug}/main.jpg`}
+            style={{ objectFit: "cover" }}
+          />
         </div>
-        <p className="mg-btm-md">{artist.bio}</p>
-        {artist.embedString ? (
-          <MusicPlayer embedString={artist.embedString} />
-        ) : null}
+
+        <div className={styles["Artist-content"]}>
+          <h1 className="mg-btm-md">{artist.name}</h1>
+          <div className="mg-btm-md">
+            <SocialLinks artist={artist} />
+          </div>
+          <p className="mg-btm-md">{artist.bio}</p>
+          {artist.embedString ? (
+            <MusicPlayer embedString={artist.embedString} />
+          ) : null}
+        </div>
       </div>
-    </div>
+    </Section>
   );
 }
