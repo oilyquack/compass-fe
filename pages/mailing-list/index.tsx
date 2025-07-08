e8d506dc0656c3e8cbca9af2bbaaf26d817cc718
Ollie Crook <olliejcrook@gmail.com>
2025-07-08 09:02:45 +0100

    Add meta content to mailing list page

--- pages/mailing-list/index.tsx
+++ pages/mailing-list/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Head from "next/head";
 
 import BrevoForm from "@/components/BrevoForm";
 
@@ -6,8 +7,17 @@ import styles from "./MailingList.module.css";
 
 export default function MailingList() {
   return (
-    <div className={styles.MailingList}>
-      <BrevoForm />
-    </div>
+    <>
+      <Head>
+        <title>Compass Music</title>
+        <meta
+          name="description"
+          content="A celebration of grassroots music, independent artists & community by the sea in Folkestone, Kent."
+        />
+      </Head>
+      <div className={styles.MailingList}>
+        <BrevoForm />
+      </div>
+    </>
   );
 }
