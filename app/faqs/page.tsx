c6f35c82db49901236852f2830ca5bec1e45e84e
Ollie Crook <olliejcrook@gmail.com>
2025-09-05 13:38:03 +0100

    Update FAQs

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -17,7 +17,11 @@ export default function FaqsPage() {
 
         <dt>Q: Where do I collect my wristband?</dt>
         <dd>
-          <b>A: </b>Open Fri 17 Oct (12pm–10pm) and Sat 18 Oct (10am–10pm).
+          <b>A: </b>At our new Festival Hub, The Workshop (32-40 Tontine Street)
+          in the heart of Folkestone’s Creative Quarter, right in the middle of
+          our participating venues!
+          <br />
+          Open Fri 17 Oct (12pm–10pm) and Sat 18 Oct (10am–10pm).
         </dd>
 
         <dt>Q: What happens if a venue is full?</dt>
@@ -30,38 +34,60 @@ export default function FaqsPage() {
 
         <dt>Q: What ticket types are available?</dt>
         <dd>
-          <b>A: </b>We also have discounted Concessions for: Students/U25s +
-          People on No/Very Low Income.
+          <b>A: </b>We have very limited <b>Super Early Bird</b> (on sale for 3
+          days only), limited Early Bird until 17 Sept, Regular, and{" "}
+          <b>Festival Support</b>/Pay It Forward if you can afford it, which
+          allow us to release more concessions.
+          <br />
+          We also have discounted <b>Concessions</b> for: Students/U25s + People
+          on No/Very Low Income.
         </dd>
 
         <dt>Q: Are there age restrictions?</dt>
         <dd>
-          <b>A: </b>Students and U25s can access discounted concessions tickets.
+          <b>A: </b>U18s (14+) must be with an adult, and most venues will only
+          allow them to stay until 9pm to comply with their license. We’d love
+          to have younger audiences still, but it adds an extra layer we cannot
+          sustain at our grassroots level.
+          <br />
+          Students and U25s can access discounted concessions tickets.
         </dd>
 
         <dt>Q: Why is everything ticketed this year?</dt>
         <dd>
-          <b>A: </b>To uphold our values, i.e. pay artists and crew fairly, have
-          good sound and keep tickets affordable, all shows need to be ticketed,
-          although our pricing is very competitive. Tickets still average less
-          than £1 per band, with concessions available.
+          <b>A: </b>Last year’s free afternoons with donations unfotunately
+          didn’t cover costs, as we only receive minimal project local funding,
+          which is also down 20% this year from last year!
+          <br />
+          To upheld our values, ie. pay artists and crew fairly, have good sound
+          and keep tickets affordable, all shows needs to be ticketed, although
+          our pricing is very competitive. Tickets still average less than £1
+          per band, with concessions available.
         </dd>
 
         <dt>Q: Will tickets be available on the door?</dt>
         <dd>
-          <b>A: </b>Our experience and those of similar organisations such as
-          the Association of Independent Promoters which we are a part of, shows
+          <b>A: </b>Possibly — but only if not sold out. Buying early helps us
+          survive and reduces risk for grassroots festivals like ours.
+          <br />
+          Our experience and those of similar organisations such as the
+          Association of Independent Promoters which we are a part of, shows
           that nearly half of ticket buyers wait until 48hr before a show to buy
-          a ticket, which sadly leads to a lot of cancellations of events, as it
-          makes planning impossible and the financial risk too high to carry for
+          a ticket, which leads sadly to a lot of cancellations of events, as it
+          makes planning impossible and the financial risk to high to carry for
           grassroots orgs!
         </dd>
 
         <dt>Q: How else can I support Compass?</dt>
         <dd>
-          <b>A: </b>Word of mouth and personal recommendations are still by far
-          the very best marketing channel for events! And we’d rather give money
-          to bands than to M*ta, quite frankly.
+          <b>A: </b>
+          Buy your ticket early if you intend to come, consider a Festival
+          Support/Pay It Forward ticket if you can afford it, or share info
+          about Compass with friends and family, and on social media !
+          <br />
+          Word of mouth and personal recommendations are still by far the very
+          best marketing channel for events! And we’d rather give money to bands
+          than to M*ta, quite frankly.
         </dd>
       </dl>
     </Section>
